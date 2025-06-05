26089d1b032f3e2d1694c029e0feb73617145a18
Jacob Zhang Jingwen <NewGuy3141@163.com>
2025-06-05 21:12:01 +0800

    Alpha 1.0 Sync

--- projects/osa/osa-client/src/main/java/top/sob/osa/client/configuration/OsaClientAutoConfiguration.java
+++ projects/osa/osa-client/src/main/java/top/sob/osa/client/configuration/OsaClientAutoConfiguration.java
@@ -2,13 +2,11 @@ package top.sob.osa.client.configuration;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Import;
 
 @Data
 @NoArgsConstructor
-@RequiredArgsConstructor
 @Import(OsaClientServiceConfiguration.class)
 @AutoConfiguration
 public class OsaClientAutoConfiguration {
