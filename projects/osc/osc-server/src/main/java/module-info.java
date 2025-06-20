634eaa35c8cf4c89476f1b36be8db3b9cce5cefc
Jacob Zhang Jingwen <NewGuy3141@163.com>
2025-06-20 22:27:58 +0800

    Alpha 1.0 Sync

--- projects/osc/osc-server/src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module osc.server {
-    requires osp.plf;
-    requires spring.boot;
-    requires spring.boot.autoconfigure;
-    requires spring.context;
-}
--- projects/osc/osc-server/src/main/java/top/sob/osc/server/configuration/OscServerConfiguration.java
+++
@@ -1,9 +0,0 @@
-package top.sob.osc.server.configuration;
-
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableAutoConfiguration
-public class OscServerConfiguration {
-}
